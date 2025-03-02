7d13e11bd6481439d4a7b444bcebb220801e65ae
rahuldeepakaiml <rahulalat@outlook.com>
2025-03-02 16:03:09 +0530

    Update app_torch.py

--- app_torch.py
+++ app_torch.py
@@ -6,15 +6,15 @@ import torch
 from sklearn.preprocessing import StandardScaler
 
 # Load the trained model
-model = torch.load("C:\\LLM\\GenAI\\annclassification\\full_model.pth", map_location=torch.device('cpu'), weights_only=False)
+model = torch.load("full_model.pth", map_location=torch.device('cpu'), weights_only=False)
 model.eval()
 
 # Load encoders and scaler
-with open('C:\\LLM\\GenAI\\annclassification\\label_encoder_gender.pkl', 'rb') as file:
+with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
-with open('C:\\LLM\\GenAI\\annclassification\\onehot_encoder_geo.pkl', 'rb') as file:
+with open('onehot_encoder_geo.pkl', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
-with open('C:\\LLM\\GenAI\\annclassification\\scaler.pkl', 'rb') as file:
+with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 # Streamlit app title
